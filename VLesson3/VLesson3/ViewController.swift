65164677f7b92b848bc8a88879c299f33c04970d
ModestSurmach <mosurmach@yandex.ru>
2023-03-09 18:38:02 +0300

    1.001

---
+++ VLesson3/VLesson3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VLesson3
+//
+//  Created by Modest Surmach on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
